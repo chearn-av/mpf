ae1192a99d3ebf91bafd1308daa896f7ff5b9116
Brandon Mar <44270980+brandon-a-mar@users.noreply.github.com>
2020-10-29 18:21:28 -0700

    Checklist Component

--- src/components/demoPage/DemoPage.jsx
+++ src/components/demoPage/DemoPage.jsx
@@ -5,6 +5,7 @@ import Dropdown from '../common/Dropdown';
 import SearchField from '../searchField/searchField';
 import CategorySearchField from '../searchField/categorySearchField';
 import Checkbox from '../common/Checkbox';
+import Checklist from '../common/Checklist';
 import RichGridDeclarativeExample from "../gridRichDataExample/RichGridDeclarativeExample";
 import './DemoPage.css';
 
@@ -19,6 +20,7 @@ function DemoPage() {
       <Textarea label='Text (Optional)' />
       <Dropdown label='Dropdown' />
       <Checkbox label='Checkbox'/>
+      <Checklist label='Checklist' list={['Option 1', 'Option 2', 'Option 3']}/>
       <div className="grid">
         <RichGridDeclarativeExample />
       </div>
